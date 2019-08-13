4fe4021f1ade93e22ce617090c44e4f5fec8e784
showroom <showroom@MacBook-Air-de-showroom.local>
2019-08-13 22:37:00 +0200

    seed debug

--- db/migrate/20190807145033_create_items.rb
+++ db/migrate/20190807145033_create_items.rb
@@ -2,7 +2,12 @@ class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
       t.string :name
-      t.text :description
+      t.string :height
+      t.string :mass
+      t.string :eye_color
+      t.string :gender
+      t.string :planet
+      t.string :species
 
       t.timestamps
     end
